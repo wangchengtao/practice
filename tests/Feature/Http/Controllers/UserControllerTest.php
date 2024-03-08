49b3da5ab283337a01d423af05f49f1e4964cf52
wangchengtao <915129420@qq.com>
2024-03-08 09:55:22 +0800

    usertest

--- tests/Feature/Http/Controllers/UserControllerTest.php
+++ tests/Feature/Http/Controllers/UserControllerTest.php
@@ -2,11 +2,23 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserControllerTest extends TestCase
 {
+    use RefreshDatabase;
 
+    public function testIndex(): void
+    {
+        Category::factory()->count(2)->create();
+
+        $this->assertDatabaseCount(Category::class, 2);
+
+        $this->get(route('categories.index'))
+             ->assertStatus(200)
+             ->assertJsonPath('data.count', 2);
+    }
 }
